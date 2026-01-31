e993112b3d4adb415078607ea25940a9d5a0bad1
denniscarr <dcarr1@gmail.com>
2026-01-31 13:37:35 -0500

    Add value to goals

--- goal/goal.gd
+++ goal/goal.gd
@@ -1,23 +1,45 @@
 class_name Goal
 extends Area2D
 
+signal collected(money: int)
 
-signal collected
+
+@export var _total_lifetime = 30.0
+@export var _grace_period = 8.0
+@export var _max_value = 100
+@export var _min_value = 1
+@export var _min_scale = 0.1
+@export var _value_label: Label
 
 
 var _is_collected: bool = false
+var _value: int = 100
+var _timer: float
 
 
 func _ready() -> void:
-    area_entered.connect(_on_area_entered)
+	area_entered.connect(_on_area_entered)
+	_timer = _total_lifetime
+	_value = _max_value
+	_value_label.text = "$%s" % _value
+
+
+func _process(delta: float) -> void:
+	_timer -= delta
+	_timer = max(0, _timer)
+	if _timer < _total_lifetime - _grace_period:
+		var t := inverse_lerp(0, _total_lifetime - _grace_period, _timer)
+		_value = ceili(lerp(_min_value, _max_value, t))
+		_value_label.text = "$%s" % _value
+		scale = Vector2.ONE * lerp(_min_scale, 1.0, t)
 
 
 func _collect():
-    visible = false
-    collected.emit()
-    _is_collected = true
+	visible = false
+	collected.emit()
+	_is_collected = true
 
 
 func _on_area_entered(area: Area2D):
-    if not _is_collected and area.get_parent() is Player:
-        _collect()
+	if not _is_collected and area.get_parent() is Player:
+		_collect()
